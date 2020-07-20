b48de56562d83ac002d23a317379ae4d5b65c540
Jheissy Pereira <jheissyp@gmail.com>
2020-07-20 15:29:40 -0500

    Updates

---
+++ precache-manifest.d6de09c3a6b95067510854d950ddb01e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e0e5ca97930d7f3129b64b02ae5de77",
+    "url": "/React-JS-acceder-y-modificar-API/index.html"
+  },
+  {
+    "revision": "249c018eb938995c15b7",
+    "url": "/React-JS-acceder-y-modificar-API/static/css/2.fc5a9450.chunk.css"
+  },
+  {
+    "revision": "3b1279706d0ccee94d5a",
+    "url": "/React-JS-acceder-y-modificar-API/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "249c018eb938995c15b7",
+    "url": "/React-JS-acceder-y-modificar-API/static/js/2.a2d702c1.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/React-JS-acceder-y-modificar-API/static/js/2.a2d702c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b1279706d0ccee94d5a",
+    "url": "/React-JS-acceder-y-modificar-API/static/js/main.fbb5a634.chunk.js"
+  },
+  {
+    "revision": "d14fb556a660117a8438",
+    "url": "/React-JS-acceder-y-modificar-API/static/js/runtime-main.16e363fa.js"
+  }
+]);
